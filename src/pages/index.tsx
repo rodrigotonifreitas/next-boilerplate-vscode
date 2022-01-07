485486350a9956f89f69fc2278ae3dd5a01739b7
rodrigotonifreitas <rodrigo.tonifreitas@gmail.com>
2022-01-07 12:50:56 -0300

    Husky Commit Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,6 @@ type Props = {
   title: string
 }
 
-const bla = 'teste'
-
 export default function Home({ title = 'Boilerplate for Next.js!' }: Props) {
   return (
     <div className={styles.container}>
